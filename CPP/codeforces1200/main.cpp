355b4171e82be55c75715417ad7958e5583d3329
jiahuiiiii <wongjiahui0915@gmail.com>
2023-04-29 13:40:17 +0800

    yay

---
+++ CPP/codeforces1200/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    while (n--) {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+    for (int i = 0; i < n; i++) {
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    int >> t;
+    while (t--) {
+        solve();
+    }
+}
